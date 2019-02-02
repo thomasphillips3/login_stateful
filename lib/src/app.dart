5243dcd6e7161e572686e4c169214fb9d848d22c
Thomas Phillips <thomasphillips3@gmail.com>
2019-02-01 17:15:41 -0800

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Log in',
+      home: Scaffold(
+        body: Text('Show a form'),
+      )
+    );
+  }
+}
